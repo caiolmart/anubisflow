8ae6a1a11e896c186884e08da34598e51dc31034
Caio Lorenzetti Martinelli <caio.martinelli@hotmail.com>
2020-05-13 02:32:12 -0300

    Add raise error tests Tuple2Node

--- tests/test_anubisflow.py
+++ tests/test_anubisflow.py
@@ -24,3 +24,17 @@ def test_tuple2_ud():
          'tot_packet_len' : int(1e10)}
     t2 = af.Tuple2Node(**k)
     assert t2.__dict__ == k
+
+def test_tuple2_raises():
+    k = {'fst_timestamp' : 5, 
+         'set_src_ports' : datetime(1995, 12, 2), 
+         'set_dst_ports' : datetime(1995, 12, 2),
+         'pkt_flag_counter' : datetime(1995, 12, 2),
+         'pkt_protocol_counter' : datetime(1995, 12, 2),
+         'tot_header_len' : datetime(1995, 12, 2),
+         'tot_packet_len' : datetime(1995, 12, 2)}
+    for item in k.items():
+        with pytest.raises(AssertionError):
+            t2 = af.Tuple2Node(**{item[0] : item[1]})
+        with pytest.raises(TypeError):
+            t2 = af.Tuple2Node(**{'foo' : 'bar'})
