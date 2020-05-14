64d191e54622ccb4aea1fc3e3d5861af035443b2
Caio Lorenzetti Martinelli <caio.martinelli@hotmail.com>
2020-05-13 22:45:33 -0300

    Add AnubisFG tests

--- tests/test_anubisflow.py
+++ tests/test_anubisflow.py
@@ -2,6 +2,7 @@ import pytest
 from datetime import datetime
 import os, pathlib
 
+from pyshark.packet.fields import LayerFieldsContainer
 import src.anubisflow as af
 
 def test_tuple2_default():
@@ -35,6 +36,45 @@ def test_tuple2_raises():
          'tot_packet_len' : datetime(1995, 12, 2)}
     for item in k.items():
         with pytest.raises(AssertionError):
-            t2 = af.Tuple2Node(**{item[0] : item[1]})
+            _ = af.Tuple2Node(**{item[0] : item[1]})
         with pytest.raises(TypeError):
-            t2 = af.Tuple2Node(**{'foo' : 'bar'})
+            _ = af.Tuple2Node(**{'foo' : 'bar'})
+
+def test_anubisfg_default():
+    afg = af.AnubisFG()
+    assert afg.memory == dict()
+
+def test_anubisfg_ud():
+    t2_1 = af.Tuple2Node()
+    ip_src_1 = LayerFieldsContainer('192.168.0.1')
+    ip_dst_1 = LayerFieldsContainer('192.168.0.2')
+    t2_2 = af.Tuple2Node()
+    ip_src_2 = LayerFieldsContainer('192.168.0.1')
+    ip_dst_2 = LayerFieldsContainer('192.168.0.2')
+    memory_1 = {(ip_src_1, ip_dst_1) : t2_1}
+    memory_2 = {(ip_src_1, ip_dst_1) : t2_1,
+                (ip_src_2, ip_dst_2) : t2_2}
+
+    afg_1 = af.AnubisFG(memory=memory_1)
+    afg_2 = af.AnubisFG(memory=memory_2)
+
+    assert memory_1 == afg_1.memory
+    assert memory_2 == afg_2.memory
+    
+def test_anubisfg_raises():
+    t2_1 = af.Tuple2Node()
+    ip_src_1 = LayerFieldsContainer('192.168.0.1')
+    ip_dst_1 = LayerFieldsContainer('192.168.0.2')
+    t2_2 = dict()
+    ip_src_2 = '192.168.0.1'
+    ip_dst_2 = '192.168.0.1'
+
+    memories = [[[ip_src_1, ip_dst_1], t2_1],
+                {ip_src_1 : t2_1},
+                {(ip_src_2, ip_dst_1) : t2_1},
+                {(ip_src_1, ip_dst_2) : t2_1},
+                {(ip_src_1, ip_dst_1) : t2_2}]
+
+    for memory in memories:
+        with pytest.raises(AssertionError):
+            _ = af.AnubisFG(memory=memory)
