90951de190af0f13aa9ba145802145b8de2a6e36
Caio Lorenzetti Martinelli <caio.martinelli@hotmail.com>
2020-05-14 05:02:40 -0300

    Add _update_twotupleuni tests

--- tests/test_anubisflow.py
+++ tests/test_anubisflow.py
@@ -2,10 +2,19 @@ import pytest
 from datetime import datetime
 import os
 import pathlib
+import pyshark
 
 from pyshark.packet.fields import LayerFieldsContainer
-from src.anubisflow import AnubisFG
+from src.anubisflow import AnubisFG, add_to_counter
 from src.nodes import TwoTupleUnidirectionalNode
+
+
+def test_add_to_counter():
+    counter = {1 : 3}
+    add_to_counter(counter, 1)
+    assert counter[1] == 4
+    add_to_counter(counter, 2)
+    assert counter[2] == 1
 
 
 def test_anubisfg_default():
@@ -48,3 +57,62 @@ def test_anubisfg_raises():
     for memory_twotup in memories:
         with pytest.raises(AssertionError):
             _ = AnubisFG(memory_twotup=memory_twotup)
+
+
+def test__update_twotupleuni_noupdate():
+    afg = AnubisFG()
+    capture = pyshark.FileCapture('tests/data/test_100_rows.pcap')
+    # First packet is a STP packet that should not be read.
+    packet = capture[0]
+
+    afg._update_twotupleuni(packet)
+    assert afg.memory_twotup == dict()
+    with pytest.raises(AttributeError, match='Attribute ip not in packet'):
+        afg._update_twotupleuni(packet, ignore_errors=False)
+
+
+def test__update_twotupleuni_update():
+    afg = AnubisFG()
+    capture = pyshark.FileCapture('tests/data/test_100_rows.pcap')
+    # Second packet is a SYN TCP packet.
+    packet = capture[1]
+
+    ip_src = LayerFieldsContainer('172.16.0.5')
+    ip_dst = LayerFieldsContainer('192.168.50.1')
+    timestamp = datetime(2018, 12, 1, 11, 17, 11, 183810)
+    src_port = 60675
+    dst_port = 80
+    protocol = 'TCP'
+    length = 74
+    pkt_flag_counter = [0] * 8
+    pkt_flag_counter[1] = 1
+
+    # Creating
+    afg._update_twotupleuni(packet)
+    expected = {'fst_timestamp': timestamp,
+                'lst_timestamp': timestamp,
+                'set_src_ports': {src_port},
+                'set_dst_ports': {dst_port},
+                'pkt_flag_counter': pkt_flag_counter,
+                'pkt_protocol_counter': {protocol : 1},
+                'tot_header_len': 0,
+                'tot_packet_len': length}
+    assert len(afg.memory_twotup) == 1
+    assert afg.memory_twotup[(ip_src, ip_dst)].__dict__ == expected
+
+    # Updating
+    # Third package is another SYN TCP packet with same IPs and Ports
+    packet = capture[2]
+    new_timestamp = datetime(2018, 12, 1, 11, 17, 11, 183813)
+    pkt_flag_counter[1] += 1
+    afg._update_twotupleuni(packet)
+    expected = {'fst_timestamp': timestamp,
+                'lst_timestamp': new_timestamp,
+                'set_src_ports': {src_port},
+                'set_dst_ports': {dst_port},
+                'pkt_flag_counter': pkt_flag_counter,
+                'pkt_protocol_counter': {protocol : 2},
+                'tot_header_len': 0,
+                'tot_packet_len': length * 2}
+    assert len(afg.memory_twotup) == 1
+    assert afg.memory_twotup[(ip_src, ip_dst)].__dict__ == expected
