88270a0fb3843761903b6d6764d1777b6f69449a
Caio Lorenzetti Martinelli <caio.martinelli@hotmail.com>
2020-05-14 02:31:18 -0300

    Add _update_twotupleuni method

--- src/anubisflow.py
+++ src/anubisflow.py
@@ -38,8 +38,8 @@ class AnubisFG:
     memory_twotup: `dict`
         The dictionary with the information of the flows. Has key (IP Source,
         IP Destination), a tuple with two
-        pyshark.packet.fields.LayerFieldsContainer's, and value TwoTupleUnidirectionalNode
-        object.
+        pyshark.packet.fields.LayerFieldsContainer's, and value 
+        TwoTupleUnidirectionalNode object.
 
     Examples
     --------
@@ -78,7 +78,7 @@ class AnubisFG:
                 assert isinstance(item[1], TwoTupleUnidirectionalNode), msg
             self.memory_twotup = memory_twotup
 
-    def update_twotuple(self, packet: Packet):
+    def _update_twotupleuni(self, packet: Packet):
         """TODO
         Usage
         -----
@@ -86,15 +86,54 @@ class AnubisFG:
         for packet in capture:
             update(packet)
         """
-        ip_src = packet.ip.src
-        ip_dst = packet.ip.dst
-        timestamp = packet.sniff_time
-        src_port = packet[packet.transport_layer].srcport
-        dst_port = packet[packet.transport_layer].dstport
-        protocol = packet.transport_layer
-        if protocol == 'TCP':
-            packet.tcp.flags_ack
-
+        try:
+            ip_src = packet.ip.src
+            ip_dst = packet.ip.dst
+            timestamp = packet.sniff_time
+            src_port = packet[packet.transport_layer].srcport
+            dst_port = packet[packet.transport_layer].dstport
+            protocol = packet.transport_layer
+            length = packet.length
+            hdr_legth = packet.ip.hdr_length
+        except:
+            return
+        # Only works for tcp packets
+        try:
+            ack = packet.tcp.flags_ack
+            cwr = packet.tcp.flags_cwr
+            ecn = packet.tcp.flags_ecn
+            fin = packet.tcp.flags_fin
+            res = packet.tcp.flags_res
+            syn = packet.tcp.flags_syn
+            urg = packet.tcp.flags_urg
+            psh = packet.tcp.flags_push
+        except:
+            ack = 0
+            cwr = 0
+            ecn = 0
+            fin = 0
+            res = 0
+            syn = 0
+            urg = 0
+            psh = 0
+        key = (ip_src, ip_dst)
+        if key in self.memory_twotup:
+            self.memory_twotup[key].lst_timestamp = timestamp
+            self.memory_twotup[key].set_src_ports.add(src_port)
+            self.memory_twotup[key].set_dst_ports.add(dst_port)
+            add_to_counter(self.memory_twotup[key].pkt_protocol_counter, 
+                           protocol)
+            self.memory_twotup[key].tot_packet_len += length
+            self.memory_twotup[key].tot_packet_len += hdr_legth
+            self.memory_twotup[key].pkt_flag_counter['ACK'] += ack
+            self.memory_twotup[key].pkt_flag_counter['CWR'] += cwr
+            self.memory_twotup[key].pkt_flag_counter['ECN'] += ecn
+            self.memory_twotup[key].pkt_flag_counter['FIN'] += fin
+            self.memory_twotup[key].pkt_flag_counter['RES'] += res
+            self.memory_twotup[key].pkt_flag_counter['SYN'] += syn
+            self.memory_twotup[key].pkt_flag_counter['URG'] += urg
+            self.memory_twotup[key].pkt_flag_counter['PSH'] += psh
+            
     def generate_features(self,
                           flow: Tuple[LayerFieldsContainer,
                                       LayerFieldsContainer]):
