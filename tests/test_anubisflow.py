34def822041bc4496e0698ff09d9046035f6b017
Caio Lorenzetti Martinelli <caio.martinelli@hotmail.com>
2020-05-15 00:59:14 -0300

    Add _generate_features_twotupleuni tests

--- tests/test_anubisflow.py
+++ tests/test_anubisflow.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import os
 import pathlib
 import pyshark
+import numpy as np
 
 from pyshark.packet.fields import LayerFieldsContainer
 from src.anubisflow import AnubisFG, add_to_counter
@@ -218,3 +219,129 @@ def test__update_twotuplebi_update():
                 'bck_tot_packet_len': length}
     assert len(afg.memory_twotup) == 1
     assert afg.memory_twotup[(ip_src, ip_dst)].__dict__ == expected
+
+
+def test__generate_features_twotupleuni():
+    '''
+        Feature list:
+            qt_pkt
+            qt_pkt_tcp
+            qt_pkt_udp
+            qt_pkt_icmp
+            qt_pkt_ip
+            qt_prtcl
+            qt_src_prt
+            qt_dst_prt
+            qt_fin_fl
+            qt_syn_fl
+            qt_psh_fl
+            qt_ack_fl
+            qt_urg_fl
+            qt_rst_fl
+            qt_ece_fl
+            qt_cwr_fl
+            avg_hdr_len
+            avg_pkt_len
+            frq_pkt
+            tm_dur
+    '''
+    n_features = 20
+    ip_src = LayerFieldsContainer('172.16.0.5')
+    ip_dst = LayerFieldsContainer('192.168.50.1')
+    key = (ip_src, ip_dst)
+    afg = AnubisFG()
+
+    # Tuple that is not on the memory.
+    empty = afg._generate_features_twotupleuni(key)
+    assert empty == [0] * n_features
+
+    # Duration 0
+    capture = pyshark.FileCapture('tests/data/test_100_rows.pcap')
+    # Second packet is a SYN TCP packet.
+    packet = capture[1]
+    timestamp = datetime(2018, 12, 1, 11, 17, 11, 183810)
+    afg._update_twotupleuni(packet)
+    expected = [
+        1, #qt_pkt
+        1, #qt_pkt_tcp
+        0, #qt_pkt_udp
+        0, #qt_pkt_icmp
+        0, #qt_pkt_ip
+        1, #qt_prtcl
+        1, #qt_src_prt
+        1, #qt_dst_prt
+        0, #qt_fin_fl
+        1, #qt_syn_fl
+        0, #qt_psh_fl
+        0, #qt_ack_fl
+        0, #qt_urg_fl
+        0, #qt_rst_fl
+        0, #qt_ece_fl
+        0, #qt_cwr_fl
+        0, #avg_hdr_len
+        74, #avg_pkt_len
+        1, #frq_pkt
+        0, #tm_dur
+    ]
+    ftrs = afg._generate_features_twotupleuni(key)
+    assert ftrs == expected
+
+    # Duration > 0
+    # Updating
+    # Third package is another SYN TCP packet with same IPs and Ports
+    packet = capture[2]
+    afg._update_twotupleuni(packet)
+    new_timestamp = datetime(2018, 12, 1, 11, 17, 11, 183813)
+    dur = (new_timestamp - timestamp).total_seconds()
+    expected = [
+        2, #qt_pkt
+        2, #qt_pkt_tcp
+        0, #qt_pkt_udp
+        0, #qt_pkt_icmp
+        0, #qt_pkt_ip
+        1, #qt_prtcl
+        1, #qt_src_prt
+        1, #qt_dst_prt
+        0, #qt_fin_fl
+        2, #qt_syn_fl
+        0, #qt_psh_fl
+        0, #qt_ack_fl
+        0, #qt_urg_fl
+        0, #qt_rst_fl
+        0, #qt_ece_fl
+        0, #qt_cwr_fl
+        0, #avg_hdr_len
+        74, #avg_pkt_len
+        2 / dur, #frq_pkt
+        dur, #tm_dur
+    ]
+    ftrs = afg._generate_features_twotupleuni(key)
+    assert ftrs == expected
+
+    # Using now datetime.
+    new_timestamp = datetime.now()
+    dur = (new_timestamp - timestamp).total_seconds()
+    expected = [
+        2, #qt_pkt
+        2, #qt_pkt_tcp
+        0, #qt_pkt_udp
+        0, #qt_pkt_icmp
+        0, #qt_pkt_ip
+        1, #qt_prtcl
+        1, #qt_src_prt
+        1, #qt_dst_prt
+        0, #qt_fin_fl
+        2, #qt_syn_fl
+        0, #qt_psh_fl
+        0, #qt_ack_fl
+        0, #qt_urg_fl
+        0, #qt_rst_fl
+        0, #qt_ece_fl
+        0, #qt_cwr_fl
+        0, #avg_hdr_len
+        74, #avg_pkt_len
+        2 / dur, #frq_pkt
+        dur, #tm_dur
+    ]
+    ftrs = afg._generate_features_twotupleuni(key, now=True)
+    assert np.isclose(ftrs, expected).all()
