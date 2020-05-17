483775a1c99d017375f657f94ea796706ff2e897
Caio Lorenzetti Martinelli <caio.martinelli@hotmail.com>
2020-05-16 23:14:21 -0300

    Add FiveTupleBidirectionalNode tests

--- tests/test_nodes.py
+++ tests/test_nodes.py
@@ -5,7 +5,7 @@ import pathlib
 
 from pyshark.packet.fields import LayerFieldsContainer
 from anubisflow.anubisflow import AnubisFG
-from anubisflow.nodes import TwoTupleUnidirectionalNode, TwoTupleBidirectionalNode, FiveTupleUnidirectionalNode
+from anubisflow.nodes import TwoTupleUnidirectionalNode, TwoTupleBidirectionalNode, FiveTupleUnidirectionalNode, FiveTupleBidirectionalNode
 
 
 def test_twotupleuni_default():
@@ -154,3 +154,64 @@ def test_fivetupleuni_raises():
             _ = FiveTupleUnidirectionalNode(**{item[0]: item[1]})
     with pytest.raises(AssertionError):
         _ = FiveTupleUnidirectionalNode(**{'foo': 'bar'})
+
+
+def test_fivetuplebi_default():
+    t5 = FiveTupleBidirectionalNode()
+    assert 0 <= (datetime.now() - t5.fst_timestamp).seconds < 5
+    assert 0 <= (datetime.now() - t5.lst_timestamp).seconds < 5
+    assert t5.fwd_tot_pkt == 0
+    assert t5.fwd_tot_header_len == 0
+    assert t5.fwd_tot_packet_len == 0
+    assert t5.fwd_max_pkt_len == 0
+    assert t5.fwd_min_pkt_len == 0
+    assert t5.fwd_tot_ttl == 0
+    assert t5.bck_tot_pkt == 0
+    assert t5.bck_tot_header_len == 0
+    assert t5.bck_tot_packet_len == 0
+    assert t5.bck_max_pkt_len == 0
+    assert t5.bck_min_pkt_len == 0
+    assert t5.bck_tot_ttl == 0
+
+
+def test_fivetuplebi_ud():
+    k = {'fst_timestamp': datetime(1995, 12, 2),
+         'lst_timestamp': datetime(1995, 12, 2),
+         'fwd_pkt_flag_counter': [10] * 8,
+         'fwd_tot_pkt': 10,
+         'fwd_tot_header_len': 1048,
+         'fwd_tot_packet_len': int(1e10),
+         'fwd_max_pkt_len': 120,
+         'fwd_min_pkt_len': 100,
+         'fwd_tot_ttl': 20,
+         'bck_pkt_flag_counter': [10] * 8,
+         'bck_tot_pkt': 10,
+         'bck_tot_header_len': 1048,
+         'bck_tot_packet_len': int(1e10),
+         'bck_max_pkt_len': 120,
+         'bck_min_pkt_len': 100,
+         'bck_tot_ttl': 20}
+    t5 = FiveTupleBidirectionalNode(**k)
+    assert t5.__dict__ == k
+
+
+def test_fivetuplebi_raises():
+    k = {'fst_timestamp': 42,
+         'lst_timestamp': 42,
+         'fwd_tot_pkt': datetime(1995, 12, 2),
+         'fwd_tot_header_len': datetime(1995, 12, 2),
+         'fwd_tot_packet_len': datetime(1995, 12, 2),
+         'fwd_max_pkt_len': datetime(1995, 12, 2),
+         'fwd_min_pkt_len': datetime(1995, 12, 2),
+         'fwd_tot_ttl': datetime(1995, 12, 2),
+         'bck_tot_pkt': datetime(1995, 12, 2),
+         'bck_tot_header_len': datetime(1995, 12, 2),
+         'bck_tot_packet_len': datetime(1995, 12, 2),
+         'bck_max_pkt_len': datetime(1995, 12, 2),
+         'bck_min_pkt_len': datetime(1995, 12, 2),
+         'bck_tot_ttl': datetime(1995, 12, 2)}
+    for item in k.items():
+        with pytest.raises(AssertionError):
+            _ = FiveTupleBidirectionalNode(**{item[0]: item[1]})
+    with pytest.raises(AssertionError):
+        _ = FiveTupleBidirectionalNode(**{'foo': 'bar'})
